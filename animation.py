a43ad4e5d52725f76ac0815b86a52ff2472767a4
tommy-swimmer <tmswimmer@fortlewis.edu>
2020-11-21 00:48:46 -0700

    fixed formatting

--- animation.py
+++ animation.py
@@ -31,18 +31,7 @@ while True:
     wn.update()
     print('It''s working')
 
-
-
 # Update November 21, 2020: using ontimer Method. 
-
-
-
-
-
-
-
-
-
 
 wn.mainloop() # This keeps the window open.
 
