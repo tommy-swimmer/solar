3b2418d2347e86594f0846496d0abb76996cf635
tommy-swimmer <tmswimmer@fortlewis.edu>
2020-11-21 00:17:55 -0700

    updated animation function

--- animation.py
+++ animation.py
@@ -12,21 +12,27 @@ wn.bgcolor("white")
 
 player = turtle.Turtle()
 player.shape("circle")
-player.color("yellow")
+player.color("black")
 
 # Define basic animation as a function
 def player_animate():
-    x = 0.5
-    player.shape("circle")
-    player.color("red")
-    time.sleep(x) # pauses program for x seconds.
-    player.shape("square") # changes the shape
-    player.color("black")
-    time.sleep(x) # pauses again.
+    if player.shape() == "square":
+        player.shape("circle")
+    elif player.shape() == "circle":
+        player.shape("square")
+    
+    # Set timer
+    wn.ontimer(player_animate, 500)
+
+player_animate()
 
 # Create a while loop to call function.
 while True:
-    player_animate()
+    wn.update()
+
+# Update November 21, 2020: using ontimer Method. 
+
+
 
 
 
--- new_solar.py
+++ new_solar.py
@@ -11,3 +11,4 @@ import numpy as np
 
 x = np.linspace(1,10,10)
 print(x)
+
