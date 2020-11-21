ad171dce7309dc04b5bf48195ae708f589a5e991
tommy-swimmer <tmswimmer@fortlewis.edu>
2020-11-20 20:09:46 -0700

    filename error

---
+++ animation.py
@@ -0,0 +1,14 @@
+# Animation Practice in Python
+# Tommy Swimmer
+# Fort Lewis College
+# November 20, 2020
+
+import turtle
+import time
+
+wn = turtle.Screen()
+wn.title("Animation Demo")
+wn.bgcolor("black")
+
+
+wn.mainloop()
