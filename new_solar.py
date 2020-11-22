aaf3995be537947c4b585742a61782ada625e369
tommy-swimmer <tmswimmer@fortlewis.edu>
2020-11-22 12:53:11 -0700

    added denver sun altitude, figure out utc

--- new_solar.py
+++ new_solar.py
@@ -7,6 +7,7 @@
 
 # Helpful Tip: Use mkdir in any directory to make a folder. rm -r to remove it.
 
+# This all works finally!
 import pysolar
 import datetime
 
@@ -15,3 +16,16 @@ date = datetime.datetime(2007, 2, 18, 15, 13, 1, 130320, tzinfo=datetime.timezon
 test = pysolar.solar.get_altitude(42.206, -71.382, date)
 
 print(test)
+#
+
+# Prototype Code
+
+# Equinox Date
+date_real = datetime.datetime(2020, 3, 20, 12, 0, 0, 0, tzinfo=datetime.timezone.utc) # figure out how to get correct time.
+
+tfr = pysolar.solar.get_altitude(39.833, -98.583, date_real) # lat and long for Denver
+
+print('Denver altitude is:', tfr)
+
+# TODO
+# Fix UTC time for Denver altitude
