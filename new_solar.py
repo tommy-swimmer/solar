a5fcf0852fc8a595b558f9cde6d375da05fec84d
tommy-swimmer <tmswimmer@fortlewis.edu>
2020-11-22 12:36:17 -0700

    working solarpy functions finally

--- new_solar.py
+++ new_solar.py
@@ -7,8 +7,11 @@
 
 # Helpful Tip: Use mkdir in any directory to make a folder. rm -r to remove it.
 
-import numpy as np
+import pysolar
+import datetime
 
-x = np.linspace(1,10,10)
-print(x)
+date = datetime.datetime(2007, 2, 18, 15, 13, 1, 130320, tzinfo=datetime.timezone.utc)
 
+test = pysolar.solar.get_altitude(42.206, -71.382, date)
+
+print(test)
