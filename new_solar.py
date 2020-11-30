13ba237ec3ccac99bcae6708b4f44e6bdaa91cac
tommy-swimmer <tmswimmer@fortlewis.edu>
2020-11-30 14:19:48 -0700

    working solar code with 2% error with NOAA

--- new_solar.py
+++ new_solar.py
@@ -10,6 +10,7 @@
 # This all works finally!
 import pysolar
 import datetime
+from datetime import timedelta
 
 date = datetime.datetime(2007, 2, 18, 15, 13, 1, 130320, tzinfo=datetime.timezone.utc)
 
@@ -20,12 +21,19 @@ print(test)
 
 # Prototype Code
 
+# Establish timedelta and timezone name
+offset = timedelta(hours=-7)
+name = 'Denver'
+
 # Equinox Date
-date_real = datetime.datetime(2020, 3, 20, 12, 0, 0, 0, tzinfo=datetime.timezone.utc) # figure out how to get correct time.
+date_real = datetime.datetime(2020, 3, 20, 12, 0, 0, 0, tzinfo=datetime.timezone(offset, name)) # figure out how to get correct time.
 
-tfr = pysolar.solar.get_altitude(39.833, -98.583, date_real) # lat and long for Denver
+denver_altitude = pysolar.solar.get_altitude(39.833, -98.583, date_real) # lat and long for Denver, answer should be around 49
+# As of 11/30/2020 got 50.2, close enough with NOAA of 49.29
 
-print('Denver altitude is:', tfr)
+print('Denver altitude is:', denver_altitude)
+print('NOAA data altitude:', 49.29)
+print('NOAA error is:', abs((denver_altitude-49.29)/denver_altitude))
 
 # TODO
 # Fix UTC time for Denver altitude
